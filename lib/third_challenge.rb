bb905d2ee2930e0243aa60194562bc0e84489a32
Nsingletary <singletarybush@gmail.com>
2020-11-22 17:00:26 -0500

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,7 +4,7 @@ epic_tragedy = {
    :hero => {:name => "Romeo", :age => "15",:status => "alive"},
    :hero_friends => []
 },
-   :capulet => {:patriarch => [:name => "Lord Capulet", :age => "50"]},
+   :capulet = {:patriarch => [:name => "Lord Capulet", :age => "50"]},
 	:matriarch => {:name => "Lady Capulet", :age => "51"},
 	:heroine => {:name => "Juliet", :age => "15", :status => "alive"},
 	:heroine_friends => []
